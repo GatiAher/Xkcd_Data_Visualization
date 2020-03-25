8ef6b2f7e21d2ae819dadf722cb81cce4a6a9bf1
GatiAher <2gatiaher@gmail.com>
2020-03-25 11:38:04 -0400

    Add Dash

---
+++ step4_interactive_plot.py
@@ -0,0 +1,14 @@
+"""
+plot points and topic labels interactivly
+
+reads from:
+* 'document_relations/tsne.npy' or 'document_relations/mds.npy'
+* 'text_vectors/serial_numbers.npy'
+* 'comic_tags/comic_tags'
+* 'comic_tags/visited_subcategories'
+
+saves to:
+* 'images/xxx.jpg'
+
+@author: Gati Aher
+"""
