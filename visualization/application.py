a91fe995b63ae0d45439986e4a369ffbb08118c8
GatiAher <2gatiaher@gmail.com>
2020-04-20 20:34:13 -0400

    Add pretty CSS padding/margin

--- visualization/application.py
+++ visualization/application.py
@@ -21,7 +21,7 @@ tsne_conv = tsne.astype(float)
 comic_serial_nums = [ str(i) for i in range(1, titles.shape[0] + 1) ]
 
 df = pd.DataFrame(data = [titles, image_urls, tsne_conv[:, 0], tsne_conv[:, 1]],
-                index=["titles", "image_urls", "x", "y"],
+                index=["title", "image_url", "x", "y"],
                 columns=comic_serial_nums
                 ).T # transpose to make rows comics and columns categories'
 
