90457f39a386f22e907784f3427d5193ae82517d
GatiAher <2gatiaher@gmail.com>
2020-04-21 20:37:54 -0400

    Clean naming conventions for hovered, selected, and picked points

--- visualization/application.py
+++ visualization/application.py
@@ -18,11 +18,13 @@ titles = np.load(root_dir + "comic_tags/titles.npy")
 image_urls = np.load(root_dir + "comic_tags/image_urls.npy")
 tsne = np.load(root_dir + "document_relations/tsne.npy")
 tsne_conv = tsne.astype(float)
-comic_serial_nums = [ str(i) for i in range(1, titles.shape[0] + 1) ]
 
-df = pd.DataFrame(data = [titles, image_urls, tsne_conv[:, 0], tsne_conv[:, 1]],
-                index=["title", "imageUrl", "x", "y"],
-                columns=comic_serial_nums
+comic_serial_nums = [ str(i) for i in range(1, titles.shape[0] + 1) ]
+comic_serial_nums = np.asarray(comic_serial_nums)
+comic_serial_nums = np.transpose(comic_serial_nums)
+
+df = pd.DataFrame(data = [titles, image_urls, tsne_conv[:, 0], tsne_conv[:, 1], comic_serial_nums],
+                index=["title", "imageUrl", "x", "y", "sn"]
                 ).T # transpose to make rows comics and columns categories'
 
 # class DataStore():
@@ -44,14 +46,16 @@ def homepage():
     chart_data = df.to_dict(orient='records')
     chart_data = json.dumps(chart_data, indent=2)
     return_chart_data = {'chart_data': chart_data}
-    return render_template('index.html', return_chart_data=return_chart_data)
+    return render_template('index.html',
+            return_chart_data=return_chart_data,
+            max_serial_num=titles.shape[0])
 
 ########
 # MAIN #
 ########
 
 if __name__ == "__main__":
-    # print("comic_serial_nums: ", len(comic_serial_nums))
+    # print("comic_serial_nums: ", comic_serial_nums.shape)
     # print("titles: ", titles.shape)
     # print("image_urls: ", image_urls.shape)
     # print("tsne: ", tsne.shape)
